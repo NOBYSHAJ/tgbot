5864bf54bc37dc30e19fd9668c7fe43ab1cd388f
NOBYSHAJ <114667776+NOBYSHAJ@users.noreply.github.com>
2022-10-07 18:30:40 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -155,7 +155,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
 
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username)),  InlineKeyboardButton(text="🤖 Make Own Admin Bot", url="https://youtu.be/W6CLKrehy6w")],
+                    [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username)),  InlineKeyboardButton(text="🤖 Make Own Admin Bot", url="Byebye")],
                      [InlineKeyboardButton(text="👥 Support Group", url="https://t.me/Cmkmedia "), InlineKeyboardButton(text="🔔 Update Channel", url="https://t.me/Cmkmedia ")],
                      [InlineKeyboardButton(text="🎬 Channel", url="@Cmkmedia "), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
 
